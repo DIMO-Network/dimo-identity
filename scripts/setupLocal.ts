207584c0ab5cb5ec44b7218989110fe05960a429
Lorran Sutter <sutterlorran@gmail.com>
2022-05-03 10:35:35 -0400

    deployment script

---
+++ scripts/setupLocal.ts
@@ -0,0 +1,63 @@
+import { ethers } from 'hardhat';
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
+
+import { DIMORegistry } from '../typechain';
+import { attributes, rootLabels } from './data';
+
+let dimoRegistry: DIMORegistry;
+
+async function deploy(admin: SignerWithAddress) {
+  console.log('\n----- Deploying DIMORegistry -----');
+  const DimoRegistryFactory = await ethers.getContractFactory('DIMORegistry');
+  dimoRegistry = await DimoRegistryFactory.connect(admin).deploy();
+
+  await dimoRegistry.deployed();
+
+  console.log('DIMORegistry deployed to:', dimoRegistry.address);
+}
+
+async function mintRoots(
+  admin: SignerWithAddress,
+  controllers: SignerWithAddress[]
+) {
+  console.log('\n----- Minting roots -----');
+  for (let i = 0; i < controllers.length; i++) {
+    await (
+      await dimoRegistry
+        .connect(admin)
+        .mintRootByOwner(rootLabels[i], controllers[i].address)
+    ).wait();
+
+    console.log(
+      `Root ${rootLabels[i]} minted with controller ${controllers[i].address}`
+    );
+  }
+}
+
+async function addAttributes(admin: SignerWithAddress) {
+  console.log('\n----- Adding attributes -----');
+  for (const attribute of attributes) {
+    const attributeBytes32 = ethers.utils.formatBytes32String(attribute);
+    await (
+      await dimoRegistry.connect(admin).addAttribute(attributeBytes32)
+    ).wait();
+
+    console.log(`Attribute ${attribute} added`);
+  }
+}
+
+async function main() {
+  const signers = await ethers.getSigners();
+
+  const admin = signers[0];
+  const controllers = signers.slice(1, 4);
+
+  await deploy(admin);
+  await mintRoots(admin, controllers);
+  await addAttributes(admin);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});

94221c29dc3399f8d93de34f5dc461ad5a2f0efe
Lorran Sutter <sutterlorran@gmail.com>
2024-01-08 23:18:32 -0500

    Fix typo

--- contracts/libraries/streamr/VehicleStreamStorage.sol
+++ contracts/libraries/streamr/VehicleStreamStorage.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.13;
 
 /**
- * @title VechielStreamStorage
+ * @title VehicleStreamStorage
  * @notice Storage of the VechielStream contract
  */
 library VehicleStreamStorage {

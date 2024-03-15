4294622469f0d371d305adc36f79b20093ed7bb6
Lorran Sutter <sutterlorran@gmail.com>
2024-03-14 22:15:21 -0400

    Revert "Mint vehicle with DD"

--- contracts/libraries/nodes/VehicleStorage.sol
+++ contracts/libraries/nodes/VehicleStorage.sol
@@ -17,8 +17,6 @@ library VehicleStorage {
         address idProxyAddress;
         // Allowed node attribute
         AttributeSet.Set whitelistedAttributes;
-        // Vehicle Id to Device Definition Id
-        mapping(uint256 => string) vehicleIdToDeviceDefinitionId;
     }
 
     /* solhint-disable no-inline-assembly */
--- utils/constants/DeviceDefinitionTable.ts
+++
@@ -1,3 +0,0 @@
-export const mockDdId1 = 'mockDdModel1_2021';
-export const mockDdId2 = 'mockDdModel2_2022';
-export const mockDdId3 = 'mockDdModel3_2023';
